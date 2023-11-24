ef3baf68852f390b735a40c4eb4ee3841ca0f423
kauasoso <129542207+kauasoso@users.noreply.github.com>
2023-11-24 10:26:12 -0300

    Update index.jsx
    .

--- src/detalhe-produto-aquecedor/aque5/index.jsx
+++ src/detalhe-produto-aquecedor/aque5/index.jsx
@@ -30,7 +30,7 @@ export default function Aque5(){
 
                     <div className='b2'>
                         <h3 className='titulo'>PLACA SOLAR ESTEIRA</h3>
-                        <h5 className='descricao'>A placa solar esteira é um coletor solar usado para aquecer a água da piscina. É um dos modelos mais comuns e conhecidos, devido ao seu baixo custo. As placas solares esteiras são instaladas sobre o telhado ou no solo, de forma a receber a máxima exposição solar. A água da piscina é bombeada para as placas, onde é aquecida pela radiação solar. A água aquecida é então devolvida à piscina, mantendo-a a uma temperatura agradável.</h5>
+                        <h5 className='descricao'>A placa solar esteira é um coletor solar usado para aquecer a água da piscina. É um dos modelos mais comuns e conhecidos, devido ao seu baixo custo. As placas solares esteiras são instaladas sobre o telhado ou no solo, de forma a receber a máxima exposição solar. A água da piscina é bombeada para as placas, onde é aquecida pela radiação solar. A água aquecida é então devolvida à piscina, mantendo-a a uma temperatura agradável</h5>
 
                         <h2 className='preco'>R$ 1246,00</h2>
 
